4d16cc500b1bf9752f94868256685b5aa671e72a
AlexG <flexorise@gmail.com>
2024-02-12 11:32:53 +0900

    static_jetton_transfer

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -1,11 +1,15 @@
 import React, {useCallback, useState} from 'react';
+import {Address} from '@ton/core';
 import ReactJson from 'react-json-view';
 import './style.scss';
+import {createTransferBody} from '../TxBuilder/MessageBuilder';
 import {SendTransactionRequest, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
 
 // In this example, we are using a predefined smart contract state initialization (`stateInit`)
 // to interact with an "EchoContract". This contract is designed to send the value back to the sender,
 // serving as a testing tool to prevent users from accidentally spending money.
+
+const JettonTransfer = createTransferBody();
 const defaultTx: SendTransactionRequest = {
 	// The transaction is valid for 10 minutes from now, in unix epoch seconds.
 	validUntil: Math.floor(Date.now() / 1000) + 600,
@@ -13,13 +17,11 @@ const defaultTx: SendTransactionRequest = {
 
 		{
 			// The receiver's address.
-			address: '0:8a5a9c7b70d329be670de4e6cce652d464765114aa98038c66c3d8ceaf2d19b0',
+			address: Address.parse('EQAunkJ4YMGPxNLs6wdDt6Ge0ryShonsJ8tAZauh0unuLT4h').toString(),
 			// Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
 			amount: '5000000',
-			// (optional) State initialization in boc base64 format.
-			stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
-			// (optional) Payload in boc base64 format.
-			payload: 'te6ccsEBAQEADAAMABQAAAAASGVsbG8hCaTc/g==',
+			// Payload for jetton transfer boc base64 format.
+			payload: JettonTransfer.toBoc().toString("base64") ,
 		},
 
 		// Uncomment the following message to send two messages in one transaction.
