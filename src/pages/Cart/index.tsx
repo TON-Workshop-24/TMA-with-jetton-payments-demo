95f03e1d2155599b4b9eb7a81e7c5a69ddee3222
krau5 <apvbd17@gmail.com>
2024-02-19 18:29:26 +0100

    Tabs & RadioGroup components, Cart address selection

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -2,6 +2,8 @@ import { Box } from '../../components/Box';
 import { Product } from '../../components/Product';
 import data from '../../assets/products.json';
 import { css } from '@emotion/react';
+import { Tabs } from '../../components/Tabs';
+import { RadioGroup } from '../../components/RadioGroup';
 
 const styles = {
   header: css`
@@ -26,6 +28,24 @@ export const Cart = () => {
           <Product product={product} quantity={9} size="dense" key={index} />
         ))}
       </Box>
+
+      <Box mt="24px" mb="16px" width="100%">
+        <Tabs
+          activeTab="1"
+          tabs={[
+            { id: '1', name: 'Pickup' },
+            { id: '2', name: 'Delivery' },
+          ]}
+        />
+      </Box>
+
+      <RadioGroup
+        name="address"
+        options={[
+          { id: 'address1', name: 'Praça Marquês de Pombal 12 A, 1250-162 Lisboa' },
+          { id: 'address2', name: 'Momma Reenstiernas Palats, Wollmar Yxkullsgatan 23, 118 50 Stockholm' }
+        ]}
+      />
     </Box>
   )
 }
