32bcbfe88f486ec81ea24296a486f27d6f805554
AlexG <39581753+Reveloper@users.noreply.github.com>
2024-03-05 21:49:33 +0900

    update-listener

--- src/components/TxComponents/JettonListener.ts
+++ src/components/TxComponents/JettonListener.ts
@@ -10,7 +10,25 @@ import {
     Transaction
 } from '@ton/ton';
 import {TupleReader} from '@ton/core';
+
+
+export async function retry<T>(fn: () => Promise<T>, options: { retries: number, delay: number }): Promise<T> {
+    let lastError: Error | undefined;
+    for (let i = 0; i < options.retries; i++) {
+        try {
+            return await fn();
+        } catch (e) {
+            if (e instanceof Error) {
+                lastError = e;
+            }
+            await new Promise(resolve => setTimeout(resolve, options.delay));
+        }
+    }
+    throw lastError;
+}
+
 export async function tryProcessJetton(orderId: string) : Promise<string> {
+
     const client = new TonClient({
         endpoint: 'https://toncenter.com/api/v2/jsonRPC',
         apiKey: '1b312c91c3b691255130350a49ac5a0742454725f910756aff94dfe44858388e',
@@ -194,3 +212,74 @@ export async function tryProcessJetton(orderId: string) : Promise<string> {
     }
     init();
 }
+
+
+
+
+
+export async function tryGetResult(exBoc: string): Promise<string> {
+    const client = new TonClient({
+        endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+        apiKey: '1b312c91c3b691255130350a49ac5a0742454725f910756aff94dfe44858388e',
+    });
+
+
+    const myAddress = Address.parse('UQCFv_2OqxdVm4IFOps-XCkW6xeug49b9FTyk8fbI-cIuj3A'); // Address to fetch transactions from
+
+
+    const timeoutPromise = new Promise<string>((_, reject) => setTimeout(() => reject(new Error('Timeout after 30 seconds')), 30000));
+
+
+    const searchPromise = new Promise<string>(async (resolve, reject) => {
+        while (true) {
+            const transactions = await client.getTransactions(myAddress, {
+                limit: 5,
+            });
+
+
+            for (const tx of transactions) {
+                const inMsg = tx.inMessage;
+                if (inMsg?.info.type === 'external-in') {
+
+
+                    const inBOC = inMsg?.body;
+                    if (typeof inBOC === 'undefined') {
+
+
+                        reject(new Error('Invalid external'));
+                        return;
+                    }
+                    const extHash = Cell.fromBase64(exBoc).hash().toString('hex')
+                    const inHash = beginCell().store(storeMessage(inMsg)).endCell().hash().toString('hex')
+
+
+                    console.log(' hash BOC', extHash);
+                    console.log('inMsg hash', inHash);
+                    console.log('checking the tx', tx, tx.hash().toString('hex'));
+
+
+
+
+                    // Assuming `inBOC.hash()` is synchronous and returns a hash object with a `toString` method
+                    if (extHash === inHash) {
+                        console.log('Tx match');
+                        const txHash = tx.hash().toString('hex');
+                        console.log(`Transaction Hash: ${txHash}`);
+                        console.log(`Transaction LT: ${tx.lt}`);
+                        resolve(txHash);
+                        return;
+                    }
+                }
+            }
+
+
+            // Add a small delay here to prevent hammering the API too hard
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    });
+
+
+    return Promise.race([searchPromise, timeoutPromise]);
+}
+
+
